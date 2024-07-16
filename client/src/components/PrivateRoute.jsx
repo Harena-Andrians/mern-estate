a96cf286f949cabeeafd042920820ea94f0c39b7
Harena-Andrians <harenaandrians@gmail.com>
2024-07-16 12:20:48 +0300

    complete profile page ui

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -3,7 +3,6 @@ import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
 export default function PrivateRoute() {
-  const currentUser = useSelector((state) => state.user.currentUser);
-  console.log(currentUser);
+  const {currentUser} = useSelector((state) => state.user);
   return currentUser ? <Outlet /> : <Navigate to="/sign-in" />;
 }
